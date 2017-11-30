63bc30fbb5793c92624f09c868679ae6f4bc83b9
WILL MEIER <willmeierart@gmail.com>
2017-11-30 15:05:49 -0700

    controlling colors with scroll position, start of imperative routing on scroll end

---
+++ components/ui/colors.js
@@ -0,0 +1,7 @@
+export default [
+  'rgba(151,13,17,1)', // AZ-RED
+  'rgba(255,255,255,1)', // white
+  'rgba(255,0,0,1)', // total red
+  'rgba(0,255,0,1)', // total yellow
+  'rgba(0,0,255,1)' // total blue
+]
--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,58 @@
 // import withData from '../lib/withData'
 // import { allFadeColors, allPaintings } from '../lib/queries'
 // import { formatColors } from '../lib/_utils'
-import App from '../components'
-import { AzLogo01 } from '../components/assets/ZeroLogos'
+// import faker from 'faker'
+// import { checkAllQueriesLoading, checkAllQueriesError } from '../lib/_utils'
+import App from '../components/App'
+import CenterLogo from '../components/layout/CenterLogo'
+
+const content = () => (
+  <div className='active'>
+    {/* {faker.lorem.paragraphs(70)} */}
+    <style jsx>{`
+      .active {
+        width: 50vw;
+        height: 100vh;
+        overflow: hidden;
+        {/* text-align: justify; */}
+      }
+    `}</style>
+  </div>
+)
 
 // include boilerplate for global loader dependent on graphql req's:
-export default () => (
-  <App title='Home'>
-    <div>
-      {/* {allThings1.loading || allThings2.loading ? (
-        <div className='loader-wrapper'>
-          <Loader type='line-spin-fade-loader' active />
-        </div>
-      ) : ( */}
-      <AzLogo01 />
-      {/* )} */}
-    </div>
-    <style jsx>{`
-        {/* .loader-wrapper {
-          width:100%; height:100%;
-          display: flex; justify-content: center; align-items:center;
+export default () => {
+  return (
+    <App title='Home'>
+      <div className='content'>
+
+        {/* const allQueries = [allThings1, allThings2]
+          checkAllQueriesLoading(allQueries, content) */}
+
+        {/* { content() } */}
+        { content() }
+        {/* <CenterLogo /> */}
+        {/* <div className='logo-mask' /> */}
+      </div>
+      <style jsx>{`
+        .content {
+          {/* background-color: var(--AZ-RED); */}
+          width:100vw;
+          height:100vh;
+        }
+        {/* .logo-mask {
+          width:100vw;
+          height:100vh;
+          position:absolute;
+          top:0; left:0;
+          display:flex;
+          justify-content:center;
+          align-items:center;
         } */}
-    `}</style>
-  </App>
-)
+      `}</style>
+    </App>
+  )
+}
 
 // example of GraphQL with multiple queries composed:
 // export default withData(
