61c176af3708c311db27e3c2db8f92af1ce9dc0a
WILL MEIER <willmeierart@gmail.com>
2017-12-07 23:04:22 -0700

    scroll config reducer working in redux, color  animations seamless

--- components/hoc/ScrollOMatic.js
+++ components/hoc/ScrollOMatic.js
@@ -1,86 +1,67 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { getRouteState, setScrollPos } from '../../lib/_redux/actions'
-// import Router from 'next/router'
-import router from '../../router'
+import { getRouteState, setScrollPos, configScrollEnv } from '../../lib/_redux/actions'
 import { fadeColor, binder } from '../../lib/_utils'
 import { setScrollState } from '../../lib/_navRules'
+import router from '../../router'
+const { Router } = router
 
 class ScrollOMatic extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      prevRoute: '',
-      nextRoute: '',
-      currentColor: this.props.routeData.bgColor1,
+      currentColor: this.props.reduxRouteData.bgColor1,
       scrollState: {},
       scrollPos: this.props.reduxRouteData.scrollPos,
       isEndOfScroll: false,
-      canScroll: false,
-      scrollTimer: null,
-      router: null
+      canScroll: false
     }
-    binder(this, ['handleScroll', 'handleClick', 'initScroll'])
-  }
-
-  async initScroll () {
-    const { scrollPos } = this.props
-    console.log(window)
-    await window.scrollTo(scrollPos.x, scrollPos.y)
-
-    // THIS IS CRUCIAL, ALLOWS FOR NAV BEYOND SAME ROUTE:
-    await setTimeout(() => {
-      console.log(this.state.canScroll)
-      window.addEventListener('scroll', this.handleScroll)
-      this.setState({ canScroll: true })
-    }, 1000)
+    binder(this, ['handleScroll', 'initScroll'])
   }
 
   componentDidMount () {
-    router.Router.prefetchRoute('main', { slug: this.props.routeData.nextRoute })
-    router.Router.prefetchRoute('main', { slug: this.props.routeData.prevRoute })
-    // setTimeout(() => {
-    //   // this.setState({ canScroll: true })
-    //   this.initScroll()
-    // }, 100)
+    // Router.prefetchRoute('main', { slug: this.props.routeData.nextRoute })
+    // Router.prefetchRoute('main', { slug: this.props.routeData.prevRoute })
+    Router.prefetchRoute(this.props.routeData.nextRoute)
+    Router.prefetchRoute(this.props.routeData.prevRoute)
 
-    const { type, bgColor1, bgColor2, subpages, prevRoute, nextRoute, route } = this.props.routeData
+    const { type, bgColor1, bgColor2, /* subpages, */ route } = this.props.routeData
 
-    if (this.props.reduxRouteData.nextRoute === ''){
+    if (this.props.reduxRouteData.nextRoute === '') {
       this.props.getRouteState(route)
+    }
+    if (type !== '') {
+      this.props.configScrollEnv(type)
     }
 
     this.initScroll()
 
     this.setState(() => ({
-      prevRoute,
-      nextRoute,
-      router: router.Router,
       currentColor: window.scrollTop > 50 ? bgColor2 : bgColor1,
       scrollState: setScrollState(type)
     }))
   }
 
-  componentWillReceiveProps (nextProps) {
-    this.initScroll()
+  // componentWillReceiveProps (nextProps) { this.initScroll()}
+
+  componentWillUnmount () { window.removeEventListener('scroll', this.handleScroll) }
+
+  async initScroll () {
+    const { scrollPos } = this.props
+    await window.scrollTo(scrollPos.x, scrollPos.y)
+    // // // // // // // // THIS IS CRUCIAL, ALLOWS FOR NAV BEYOND SAME ROUTE:
+    await setTimeout(() => {
+      window.addEventListener('scroll', this.handleScroll)
+      this.setState({ canScroll: true })
+    }, 1000)
   }
 
-  componentWillUnmount () {
-    // this.setState({canScroll: false})
-    window.removeEventListener('scroll', this.handleScroll)
-  }
   handleScroll (e) {
     e.preventDefault()
-    const { router } = this.state
     const { routeData: { bgColor1, bgColor2 } } = this.props
     let { scrollTop, scrollHeight } = e.srcElement.scrollingElement
     scrollHeight = scrollHeight / 2
     const scrollTiplier = scrollTop / scrollHeight
-    // console.log(scrollTop)
-
-    // if (!this.state.canScroll && scrollTop === 0) {
-    //   window.scrollTo(0, 5)
-    // }
 
     this.setState({
       currentColor: fadeColor(scrollTiplier, [bgColor1, bgColor2])
@@ -88,48 +69,29 @@ class ScrollOMatic extends Component {
 
     if (this.state.canScroll) {
       if (scrollTop === 0) {
-        this.setState({ canScroll: false })        
-        router.pushRoute('main', { slug: this.state.prevRoute })
+        this.setState({ canScroll: false })
+        this.props.getRouteState(this.props.routeData.prevRoute)
+        Router.pushRoute('main', { slug: this.props.routeData.prevRoute })
         this.props.setScrollPos({ x: 0, y: scrollHeight - 1 })
-        // window.scrollTo(0, scrollHeight - 1)
       }
 
-    // else {
-    //   window.scrollTo(0, 5)
-    //   this.setState({ canScroll: true })
-    // }
       if (scrollTiplier === 1) {
-        this.setState({ canScroll: false })        
-        console.log(router)
+        this.setState({ canScroll: false })
         this.props.getRouteState(this.props.routeData.nextRoute)
-        router.pushRoute('main', { slug: this.props.routeData.nextRoute })
+        Router.pushRoute('main', { slug: this.props.routeData.nextRoute })
         this.props.setScrollPos({ x: 0, y: 1 })
-        // window.scrollTo(0, 1)
       }
     }
   }
-  handleClick () {
-    // console.log(router)
-    this.setState({ canScroll: false })
-    
-    const { router } = this.state
-    console.log(this.props)
-    // console.log(this.state.router)
-    // this.props.getRouteState()
-    this.props.getRouteState(this.props.routeData.nextRoute)
-    this.props.setScrollPos({ x: 0, y: 1 })
-    router.pushRoute('main', { slug: this.props.routeData.nextRoute })
-    // router.pushRoute('main', { slug: this.state.prevRoute })
-  }
+
   render () {
     return (
-      <div className='scroll-o-matic' onClick={this.handleClick}>
+      <div style={{ backgroundColor: this.state.currentColor }} className='scroll-o-matic'>
         {this.props.children}
         <style jsx>{`
           .scroll-o-matic {
             width:100vw;
             height:200vh;
-            background-color: ${this.state.currentColor};
           }
         `}</style>
       </div>
@@ -140,22 +102,24 @@ class ScrollOMatic extends Component {
 function mapStateToProps (state) {
   return {
     reduxRouteData: state.nav.routeData,
-    scrollPos: state.nav.scrollPos
+    scrollPos: state.nav.scrollPos,
+    scrollConfig: state.nav.scrollConfig
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return {
-    getRouteState: (route) => { dispatch(getRouteState(route)) },
-    setScrollPos: (coords) => {
-      window.scrollTo(coords.x, coords.y)
+    getRouteState: route => dispatch(getRouteState(route)),
+    setScrollPos: coords => {
+      // console.log(coords)
+      // console.log(window)
+      // console.log(window.scrollTo)
+      // console.log(window.scrollTo(coords.x, coords.y))
+      // window.scrollTo(coords.x, coords.y)
       dispatch(setScrollPos(coords))
-    }
+    },
+    configScrollEnv: type => dispatch(configScrollEnv(type))
   }
 }
 
-export default connect(
-  mapStateToProps,
-  // { getRouteState, setScrollPos }
-  mapDispatchToProps
-)(ScrollOMatic)
+export default connect(mapStateToProps, mapDispatchToProps)(ScrollOMatic)
