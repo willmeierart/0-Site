5c8fee17e2b721b84d7a940ba750d78d26d41b8c
WILL MEIER <willmeierart@gmail.com>
2018-01-05 16:35:28 -0700

    big moves, transitions and color change working, need to disable elastic scroll effects

--- components/nav/MobileScrollOMatic.js
+++ components/nav/MobileScrollOMatic.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import throttle from 'lodash.throttle'
+import _ from 'lodash'
 import { transitionRoute, setColorScheme, setPrevNextRoutes, getNewOriginPos } from '../../lib/redux/actions'
 import { binder, fadeColor } from '../../lib/_utils'
 import router from '../../router'
@@ -10,10 +10,10 @@ class MobileScrollOMatic extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      current: 1,
-      canScroll: false
+      canScroll: false,
+      scrollTiplier: 1
     }
-    binder(this, ['handleScroll', 'navigator'])
+    binder(this, ['handleScroll', 'navigator', 'changeColors'])
   }
 
   componentDidMount () {
@@ -44,71 +44,64 @@ class MobileScrollOMatic extends Component {
     window.removeEventListener('touchmove', (e) => { this.handleScroll(e) })
   }
 
+  changeColors () {
+    const { routeData: { bgColor1, bgColor2 }, setColorScheme } = this.props
+    const { scrollTiplier } = this.state
+    setColorScheme({
+      cur1: fadeColor(scrollTiplier, [bgColor1, bgColor2]),
+      cur2: fadeColor(scrollTiplier, [bgColor2, bgColor1])
+    })
+  }
+
   navigator (e) {
-    const { routeData: { type }, getNewOriginPos, transitionRoute, prevNextRoutes: { nextRoute, prevRoute } } = this.props
+    const { routeData: { type, navRules: { style: { height } } }, getNewOriginPos, transitionRoute, prevNextRoutes: { nextRoute, prevRoute } } = this.props
     const { innerWidth, innerHeight } = window
-    const widthHeight = [innerWidth, innerHeight]
-    const scrollTop = typeof e.srcElement.scrollingElement.scrollTop !== 'undefined' ? e.srcElement.scrollingElement.scrollTop : 0
-    const scrollHeight = typeof e.srcElement.scrollingElement.scrollHeight !== 'undefined' ? e.srcElement.scrollingElement.scrollHeight : 0
-
+    const { scrollTop, scrollHeight } = e.srcElement.scrollingElement
     const thisScrollHeight = innerHeight + scrollTop - 1
-
     const prevTrigger = scrollTop <= 0
     const nextTrigger = scrollHeight - thisScrollHeight - 1 <= 0
-
     const routerData = { prevTrigger, nextTrigger, prevRoute, nextRoute }
+    const that = this
+
+    const transitionDirection = (dir) => {
+      console.log('transitionDirection')
+      const route = dir === 'forward' ? nextRoute : prevRoute
+      getNewOriginPos(route, dir, [innerWidth, innerHeight])
+      window.removeEventListener('scroll', (e) => { that.handleScroll(e) })
+      window.removeEventListener('touchmove', (e) => { that.handleScroll(e) })
+      transitionRoute(routerData)
+      that.setState({ canScroll: false })
+    }
+
+    const scrollTiplier = ((scrollTop / (scrollHeight - scrollTop)) / 2).toFixed(3)
+
+    this.setState({ scrollTiplier })
     if ((nextTrigger || prevTrigger) && this.state.canScroll) {
-      console.log(scrollTop)
-      const that = this
-      const doTransitionStuff = (dir) => {
-        const route = dir === 'forward' ? nextRoute : prevRoute
-        getNewOriginPos(route, dir, widthHeight)
-        console.log(dir)
-        console.log({
-          thisScrollHeight,
-          scrollHeight
-        })
-        window.removeEventListener('scroll', (e) => { that.handleScroll(e) })
-        window.removeEventListener('touchmove', (e) => { that.handleScroll(e) })
-        transitionRoute(routerData)
-        that.setState({ canScroll: false })
-      }
-      switch (true) {
-        case nextTrigger :
-          doTransitionStuff('forward')
-          break
-        case prevTrigger :
-          doTransitionStuff('back')
-          break
-        default:
-          return null
-      }
+      const doTransitionThings = () => nextTrigger ? transitionDirection('forward') : transitionDirection('back')
+      const throttled = _.throttle(doTransitionThings, 800, { 'leading': true })
+      throttled()
     }
   }
 
   handleScroll (e) {
-    const { current, canScroll } = this.state
-    const { setColorScheme, routeData: { bgColor1, bgColor2 } } = this.props
+    const { canScroll } = this.state
+    const { routeData: { bgColor1, bgColor2 } } = this.props
 
-    setColorScheme({
-      cur1: fadeColor(current, [bgColor1, bgColor2]),
-      cur2: fadeColor(current, [bgColor2, bgColor1])
-    })
     if (canScroll) {
-      throttle(this.navigator(e), 1000)
+      this.navigator(e)
+      this.changeColors()
     }
   }
 
   render () {
-    const { currentColor } = this.state
-    const { routeData: { type, navRules: { style: { height } } } } = this.props
+    const { colors: { cur1 }, routeData: { type, navRules: { style: { height } } } } = this.props
 
     return (
       <div className='mobile-matic' style={{
         width: '100vw',
         height: `${Math.floor(height * 100)}vh`,
         boxSizing: 'border-box',
-        backgroundColor: currentColor
+        backgroundColor: cur1
         // overflowScrolling: 'touch',
         // WebkitOverflowScrolling: 'touch'
       }}>
